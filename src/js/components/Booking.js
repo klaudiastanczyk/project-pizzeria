fb497de31948fada4f7a7df1d9365715b07a0c4c
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-05-11 18:23:55 +0200

    add booking date and hour

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 
 class Booking{
@@ -19,6 +21,9 @@ class Booking{
 
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
+    
   }
 
   initWidgets(){
@@ -26,6 +31,11 @@ class Booking{
 
     thisBooking.people = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hours = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.date = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hour = new HourPicker(thisBooking.dom.hourPicker);
+
+
+
   }
 }
 
